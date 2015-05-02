6631b6348f53ae4c411ff5926a43e9bfd3f6baf7
Smadey Zhang <365649304@qq.com>
2015-05-02 20:17:12 +0800

    优化加入之后显示页面

--- src/entity/joined/index.js
+++ src/entity/joined/index.js
@@ -5,16 +5,25 @@ $(function() {
     var entityId = urlParams[1];
     var postId = urlParams[3];
 
-    $.getJSON('/services/entity/hot', function(result) {
+    $.getJSON('/services/entity/' + entityId, function(result) {
         if(result.status == 'success') {
-            initHotEntitys(result.data);
+            initEntity(result.data);
         }
         else {
             console.error(result.data);
         }
     });
 
-    function initHotEntitys(entitys) {
+    $.getJSON('/services/entity/hot', function(result) {
+        if(result.status == 'success') {
+            initHotList(result.data);
+        }
+        else {
+            console.error(result.data);
+        }
+    });
+
+    function initEntity(entity) {
         var backUrl = location.origin + '/entity/' + entityId + '?pid=' + postId;
 
         // 微信分享
@@ -30,10 +39,12 @@ $(function() {
         $('.back').on('click', function() {
             location.href = backUrl;
         });
+    }
 
+    function initHotList(entitys) {
         $('.hot-list').html(template('hot-list-template', { entitys: entitys }));
 
-        var swiper = new Swiper('.swiper-container', {
+        new Swiper('.swiper-container', {
             direction: 'horizontal',
             slidesPerView: 'auto',
             spaceBetween: 20,
